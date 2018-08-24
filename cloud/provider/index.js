a723d4a55e829f3aae65d16ef58475b057fa641a
Pehuen Rodriguez <pehuen.rodriguez@gmail.com>
2018-08-24 13:54:07 -0300

    agrego método parseXubio

--- cloud/provider/index.js
+++ cloud/provider/index.js
@@ -2,23 +2,16 @@ var xubioService = require('../../services/xubio')
 var Provider = require('../../models/provider.js')
 
 module.exports = {
-
   providerSync: (req, res) => {
-    // basic validations
-    //if (!req.params.providers.length) {
-    //  res.error('No providers received')
-    //}
-
     // get xubio token. xubio.service
     xubioService.credential.getToken()
       .then(token => {
         // get all providers from Xubio
         xubioService.provider.getAll(token.token_type, token.access_token)
           .then(providers => {
-              processSync(token, providers,res)
-              //processExp(token, providers,res)
-          }).catch()
-
+            processSync(token, providers, res)
+          })
+          .catch()
       })
       .catch(error => {
         console.log(error)
@@ -26,56 +19,47 @@ module.exports = {
   }
 }
 
-//not used
-function getAllParseProviders () {
-    parseProviders().then( providers => {
-         return providers
-    }).catch( error => {console.log(error)});
+// not used
+function getAllParseProviders() {
+  parseProviders().then(providers => {
+    return providers
+  }).catch(error => {
+    console.log(error)
+  })
 }
 
-//look for parse Provider by externalId
-async function getProviderById (id) {
-  //console.log(token)
+// look for parse Provider by externalId
+async function getProviderByExternalId(id) {
   let query = new Parse.Query('Provider')
-  var res =
-  query.doesNotExist('deletedAt')
+  var res = query.doesNotExist('deletedAt')
   query.equalTo("externalId", id)
   return query.find()
 }
 
-
 // update provider received in Parse
-//**not used yet
-function updateProvider (provider) {
+// not used
+function updateProvider(provider) {
   let myProvider = Provider.createFromXubio(provider)
-  // check if Provider exists: update or save. models.provider
-  // let query = new Parse.Query("Provider");
-  // query.equalTo('externalId', provider.get('externalId'));
   return myProvider.save()
 }
 
-//experimental
-async function processSync (token, providers, res) {
-  var list = []
+async function processSync(token, providers, res) {
+  let list = []
+  let xubio = null
 
   for (const item of providers) {
-
-      results =  await getProviderById(item.proveedorid)
-
-      if (results.length === 0) {
-          console.log("ID "+item.proveedorid+"  no está en courier lo sincronizamos")
-          list.push(item)
-      } else {
-          //get all fields from Provider - xubioService
-          let xubio = await getXubioPovider(item.proveedorid, token)
-          // check if all field are equals
-          if (providerEquals(xubio, results[0])) {
-              console.log("This item "+xubio.proveedorid+" "+xubio.nombre+" is already synched")
-          } else {
-              list.push(item)
-              console.log("This item "+xubio.proveedorid+" "+xubio.nombre+" has to be updated")
-          }
+    results = await getProviderByExternalId(item.proveedorid)
+    if (results.length === 0) {
+      xubio = await getXubioPovider(item.proveedorid, token)
+      list.push(castProvider(xubio))
+    } else {
+      //get all fields from Provider - xubioService
+      xubio = await getXubioPovider(item.proveedorid, token)
+      // check if all field are equals
+      if (!providerEquals(xubio, results[0])) {
+        list.push(castProvider(xubio))
       }
+    }
   }
   // send a response with all the providers not synched with xubio (new and not updated)
   res.success(list)
@@ -83,39 +67,47 @@ async function processSync (token, providers, res) {
 
 //get the provider from xubio async
 async function getXubioPovider(proveedorId, token) {
-    return xubioService.provider.getById(proveedorId, token.token_type, token.access_token)
+  return xubioService.provider.getById(proveedorId, token.token_type, token.access_token)
 }
 
 //check if both providers are equal
-function providerEquals (xubio, courier) {
-     //console.log(" "+JSON.stringify(courier.get("province")))
-     //console.log(" "+JSON.stringify(xubio.provincia))
+function providerEquals(xubio, courier) {
+  if (courier.get("externalId") !== xubio.proveedorid) return false
+  if (courier.get("name") !== xubio.nombre) return false
+  if (courier.get("taxCategory") !== xubio.categoriaFiscal ? xubio.categoriaFiscal.ID : null) return false // categoría, condición frente al IVA
+  if (courier.get("docType") !== xubio.identificacionTributaria ? xubio.identificacionTributaria.ID : null) return false // CUIT, DNI, etcétera
+  if (courier.get("docValue") !== xubio.CUIT) return false // número de DNI, CUIT, etcétera
+  if (courier.get("purchaseAccount") !== xubio.cuentaCompra_id ? xubio.cuentaCompra_id.ID : null) return false
+  if (courier.get("saleAccount") !== xubio.cuentaVenta_id ? xubio.cuentaCompra_id.ID : null) return false
+  if (courier.get("country") !== xubio.pais ? xubio.pais.ID : null) return false // va a requerir MAP
+  if (courier.get("province") !== xubio.provincia ? xubio.provincia.ID : null) return false // va a requerir MAP
+  if (courier.get("address") !== xubio.direccion) return false
+  if (courier.get("postalCode") !== xubio.codigoPostal) return false
+  if (courier.get("email") !== xubio.email) return false
+  if (courier.get("observation") !== xubio.observaciones) return false
+  if (courier.get("businessName") !== xubio.razonSocial) return false
+  if (courier.get("phone") !== xubio.telefono) return false
+  if (courier.get("userCode") !== xubio.usrCode) return false
+  return true
+}
 
-    if(courier.get("vatId") != xubio.CUIT) return false
-
-    if(JSON.stringify(courier.get("taxCategory")) != JSON.stringify(xubio.categoriaFiscal)) return false
-
-    if(courier.get("postalCode") != xubio.codigoPostal) return false
-
-    if(JSON.stringify(courier.get("purchaseAccount")) != JSON.stringify(xubio.cuentaCompra_id))return false
-    if(JSON.stringify(courier.get("saleAccount")) != JSON.stringify(xubio.cuentaVenta_id)) return false
-
-    if(courier.get("address") != xubio.direccion) return false
-    if(courier.get("email") != xubio.email) return false
-
-    if(JSON.stringify(courier.get("taxType")) != JSON.stringify(xubio.identificacionTributaria)) return false
-
-    if(courier.get("name") != xubio.nombre) return false
-    if(courier.get("observation") != xubio.observaciones) return false
-
-    if(JSON.stringify(courier.get("country")) != JSON.stringify(xubio.pais)) return false
-
-    if(courier.get("externalId") != xubio.proveedorid) return false
-
-    if(JSON.stringify(courier.get("province")) != JSON.stringify(xubio.provincia)) return false
-
-    if(courier.get("businessName") != xubio.razonSocial) return false
-    if(courier.get("phone") != xubio.telefono) return false
-    if(courier.get("userCode") != xubio.usrCode) return false
-    return true
+function castProvider(xubio) {
+  let provider = {}
+  provider.externalId = xubio.proveedorid
+  provider.name = xubio.nombre
+  provider.taxCategory = xubio.categoriaFiscal ? xubio.categoriaFiscal.ID : null
+  provider.docType = xubio.identificacionTributaria ? xubio.identificacionTributaria.ID : null
+  provider.docValue = xubio.CUIT
+  provider.purchaseAccount = xubio.cuentaCompra_id ? xubio.cuentaCompra_id.ID : null
+  provider.saleAccount = xubio.cuentaVenta_id ? xubio.cuentaVenta_id.ID : null
+  provider.country = xubio.pais ? xubio.pais.ID : null // va a requerir MAP
+  provider.province = xubio.provincia ? xubio.provincia.ID : null // va a requerir MAP
+  provider.address = xubio.direccion
+  provider.postalCode = xubio.codigoPostal
+  provider.email = xubio.email
+  provider.observation = xubio.observaciones
+  provider.businessName = xubio.razonSocial
+  provider.phone = xubio.telefono
+  provider.userCode = xubio.usrCode
+  return provider
 }
