d847faebbc71ca254e31dce383339c1f30e4e7a2
Tobias <tirigoyen@nayra.com.ar>
2018-08-28 19:25:45 +0200

    [CA-16] listamos los clientes nuevos de Xubio

--- cloud/client/index.js
+++ cloud/client/index.js
@@ -3,14 +3,14 @@ var Client = require('../../models/client.js')
 
 module.exports = {
   clientSync: (req, res) => {
-    // get xubio token. xubio.service
-    xubioService.credential.getToken()
-      .then(token => {
-        // get all providers from Xubio
-        xubioService.client.getAll(token.token_type, token.access_token)
-          .then(clients => {
-              //res.success(clients)
-              processSync(token, clients, res)
+     // get xubio token. xubio.service
+     xubioService.credential.getToken()
+       .then(token => {
+        // get all clients from Xubio
+          xubioService.client.getAll(token.token_type, token.access_token)
+            .then(clients => {
+               //res.success(clients)
+               processSync(token, clients, res)
           })
           .catch()
       })
@@ -28,29 +28,29 @@ async function getClientByExternalId(id) {
   return query.find()
 }
 
-
+// looking for new clients from xubio
 async function processSync(token, clients, res) {
   let list = []
   let xubio = null
 
   for (const item of clients) {
     results = await getClientByExternalId(item.cliente_id)
+
     if (results.length === 0) {
-        xubio = await getXubioClient(item.cliente_id, token)
-        //list.push(castProvider(xubio))
-        list.push(xubio)
+      xubio = await getXubioClient(item.cliente_id, token)
+      list.push(castClient(xubio))
     }
     // else {
-    //   //get all fields from Provider - xubioService
-    //   xubio = await getXubioPovider(item.proveedorid, token)
+    //   //get all fields from Client - xubioService
+    //   xubio = await getXubioClient(item.proveedorid, token)
     //   // check if all field are equals
-    //   if (!providerEquals(xubio, results[0])) {
-    //     list.push(castProvider(xubio))
+    //   if (!clientEquals(xubio, results[0])) {
+    //     list.push(castClient(xubio))
     //   }
     // }
-  }
-  // send a response with all the providers not synched with xubio (new and not updated)
-  res.success(list)
+   }
+    // send a response with all the clients not synched with xubio (new)
+   res.success(list)
 }
 
 //get the client from xubio async
@@ -58,44 +58,23 @@ async function getXubioClient(clientId, token) {
   return xubioService.client.getById(clientId, token.token_type, token.access_token)
 }
 
-//check if both providers are equal
-function clientEquals(xubio, courier) {
-  if (courier.get("externalId") !== xubio.proveedorid) return false
-  if (courier.get("name") !== xubio.nombre) return false
-  if (courier.get("taxCategory") !== (xubio.categoriaFiscal ? xubio.categoriaFiscal.ID : null)) return false // categoría, condición frente al IVA
-  if (courier.get("docType") !== (xubio.identificacionTributaria ? xubio.identificacionTributaria.ID : null)) return false // CUIT, DNI, etcétera
-  if (courier.get("docValue") !== xubio.CUIT) return false // número de DNI, CUIT, etcétera
-  if (courier.get("purchaseAccount") !== (xubio.cuentaCompra_id ? xubio.cuentaCompra_id.ID : null)) return false
-  if (courier.get("saleAccount") !== (xubio.cuentaVenta_id ? xubio.cuentaVenta_id.ID : null)) return false
-  if (courier.get("country") !== (xubio.pais ? xubio.pais.ID : null)) return false // va a requerir MAP
-  if (courier.get("province") !== (xubio.provincia ? xubio.provincia.ID : null)) return false // va a requerir MAP
-  if (courier.get("address") !== xubio.direccion) return false
-  if (courier.get("postalCode") !== xubio.codigoPostal) return false
-  if (courier.get("email") !== xubio.email) return false
-  if (courier.get("observation") !== xubio.observaciones) return false
-  if (courier.get("businessName") !== xubio.razonSocial) return false
-  if (courier.get("phone") !== xubio.telefono) return false
-  if (courier.get("userCode") !== xubio.usrCode) return false
-  return true
-}
-
-function castProviderClient(xubio) {
-  let provider = {}
-  provider.externalId = xubio.proveedorid
-  provider.name = xubio.nombre
-  provider.taxCategory = (xubio.categoriaFiscal ? xubio.categoriaFiscal.ID : null)
-  provider.docType = (xubio.identificacionTributaria ? xubio.identificacionTributaria.ID : null)
-  provider.docValue = xubio.CUIT
-  provider.purchaseAccount = (xubio.cuentaCompra_id ? xubio.cuentaCompra_id.ID : null)
-  provider.saleAccount = (xubio.cuentaVenta_id ? xubio.cuentaVenta_id.ID : null)
-  provider.country = (xubio.pais ? xubio.pais.ID : null) // va a requerir MAP
-  provider.province = (xubio.provincia ? xubio.provincia.ID : null) // va a requerir MAP
-  provider.address = xubio.direccion
-  provider.postalCode = xubio.codigoPostal
-  provider.email = xubio.email
-  provider.observation = xubio.observaciones
-  provider.businessName = xubio.razonSocial
-  provider.phone = xubio.telefono
-  provider.userCode = xubio.usrCode
-  return provider
+function castClient(xubio) {
+  let client = {}
+  client.externalId = xubio.cliente_id
+  client.docValue = xubio.CUIT
+  client.taxCategory = (xubio.categoriaFiscal ? xubio.categoriaFiscal.ID : null)
+  client.purchaseAccount = (xubio.cuentaCompra_id ? xubio.cuentaCompra_id.ID : null)
+  client.saleAccount = (xubio.cuentaVenta_id ? xubio.cuentaVenta_id.ID : null)
+  client.address = xubio.direccion
+  client.email = xubio.email
+  client.docType = (xubio.identificacionTributaria ? xubio.identificacionTributaria.ID : null)
+  client.name = xubio.nombre
+  client.country = (xubio.pais ? xubio.pais.ID : null) // va a requerir MAP
+  client.province = (xubio.provincia ? xubio.provincia.ID : null) // va a requerir MAP
+  client.postalCode = xubio.codigoPostal
+  client.observation = xubio.observaciones
+  client.businessName = xubio.razonSocial
+  client.phone = xubio.telefono
+  client.userCode = xubio.usrCode
+  return client
 }
