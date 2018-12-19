340950043043df3f858567d98e54de19f6ae1ead
Pehuen Rodriguez <pehuen.rodriguez@gmail.com>
2018-12-18 22:02:15 -0300

    Moved users crud to cloud

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,13 @@
-let client = require('./client')
-let provider = require('./provider')
-let ups = require('./ups')
+const client = require('./client')
+const provider = require('./provider')
+const ups = require('./ups')
+const user = require('./user')
 
 Parse.Cloud.define('ClientSync', client.clientSync)
 Parse.Cloud.define('ProviderSync', provider.providerSync)
 Parse.Cloud.define('UpsLabelRecovery', ups.labelRecovery)
+
+Parse.Cloud.define('CreateUser', user.createUser)
+Parse.Cloud.define('GetAllUsers', user.getAllUsers)
+Parse.Cloud.define('GetUserById', user.getUserById)
+Parse.Cloud.define('UpdateUser', user.updateUser)
