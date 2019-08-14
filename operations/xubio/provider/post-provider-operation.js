f9041785cdb2fa49e31ced41f78263d837501493
Ignacio <ignacio.guerra@nayra.coop>
2019-08-14 09:20:31 -0300

    Post provider auth

--- operations/xubio/provider/post-provider-operation.js
+++ operations/xubio/provider/post-provider-operation.js
@@ -1,13 +1,20 @@
 /* eslint camelcase: 0 */
 var rp = require('request-promise')
 const { PROVIDER_BEAN_PATH } = require('../beanPaths')
+const { getToken } = require('../security/get-token-operation')
 
 // @token
 // + token_type string
 // + access_token string
 // @provider to be saved in Xubio
 // returns xubio provider created
-const postProvider = async ({ token_type, access_token }, provider) => {
+const postProvider = async (provider, token = null) => {
+  let xubioToken = token
+
+  if (!token) {
+    xubioToken = await getToken()
+  }
+  const { token_type, access_token } = xubioToken
   const xubioApiProviderEndpoint = `${process.env.XUBIO_API_URI}${PROVIDER_BEAN_PATH}`
   const headers = {
     'Authorization': token_type + ' ' + access_token,
@@ -18,14 +25,10 @@ const postProvider = async ({ token_type, access_token }, provider) => {
     method: 'POST',
     headers,
     body: provider,
-    auth: {
-      'user': process.env.XUBIO_CLIENT_ID,
-      'pass': process.env.XUBIO_SECRET
-    },
     json: true
   }
-  const resProvider = await rp(options)
-  return resProvider
+
+  return rp(options)
 }
 
 module.exports = {
